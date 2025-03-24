46b03e4a51a97bcee263c936fb5c3be758cd4b74
unknown <diogocastrorosas@gmail.com>
2025-03-24 00:15:24 -0500

    Cambios

---
+++ Lab2_actividades/src/GeneAct1/Principal.java
@@ -0,0 +1,28 @@
+package GeneAct1;
+
+public class Principal {
+	public static void main(String[] args) {
+		Bolsa < Chocolatina > bolsaCho = new Bolsa < Chocolatina > (3);
+		Chocolatina c = new Chocolatina("milka");
+		Chocolatina c1 = new Chocolatina("milka");
+		Chocolatina c2 = new Chocolatina("ferrero");
+		bolsaCho.add(c);
+		bolsaCho.add(c1);
+		bolsaCho.add(c2);
+		for (Chocolatina chocolatina: bolsaCho) {
+			System.out.println(chocolatina.getMarca());
+		}
+		
+		Bolsa < Golosina > bolsaGo = new Bolsa < Golosina > (3);
+		Golosina g = new Golosina("golosina1", 0);
+		Golosina g1 = new Golosina("golosina2", 1);
+		Golosina g2 = new Golosina("golosina3", 2);
+		bolsaGo.add(g);
+		bolsaGo.add(g1);
+		bolsaGo.add(g2);
+		for (Golosina go: bolsaGo) {
+			System.out.println(go.getNombre() + " " + go.getPeso());
+		}
+	}
+}
+	
